fbdbbbbb4831375fdf801364ef69f532e099f266
Mubarizt44 <130185672+Mubarizt44@users.noreply.github.com>
2025-12-04 18:24:45 +0000

    Add files via upload

---
+++ Employee_overview/Project_overview.sql
@@ -0,0 +1,39 @@
+-- project status
+
+with project_status as (
+select
+project_id,
+project_name,
+project_budget,
+'Upcoming' as status
+from [upcoming projects]
+union all
+select 
+project_id,
+project_name,
+project_budget,
+'Completed' as status
+from completed_projects)
+
+
+
+
+
+-- main table 
+select
+e.employee_id,
+e.first_name,
+e.last_name,
+e.job_title,
+e.salary,
+d.Department_Name,
+pa.project_id,
+p.project_name,
+p.status
+from employees e
+join  departments d
+on e.department_id = d.Department_ID
+join project_assignments pa
+on pa.employee_id = e.employee_id
+join project_status p 
+on p.project_id = pa.project_id 
